4f029ac27b1865accf73c9c9a4a32a8bd8ce38b1
Vinay Jeurkar <vjeurkar03@gmail.com>
2022-04-20 21:11:56 +0530

    Added example for echo usecase

---
+++ cmd/04_echoCmd.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var EchoCmd = &cobra.Command{
+	Use:   "echo [strings to echo]",
+	Short: "prints given strings to stdout",
+	Run: func(c *cobra.Command, args []string) {
+		fmt.Println("Echo: ", strings.Join(args, " "))
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(EchoCmd)
+}
